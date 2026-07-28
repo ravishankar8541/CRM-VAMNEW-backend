75f8aa87f5e6230b6a3d6deb5739c35bddd9cf61
unknown <ravishankarray480@gmail.com>
2026-07-28 14:40:33 +0530

    first init

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 dbConnection();
 
 app.use(cors({
-  origin: ['http://localhost:5175', 'https://www.viralcrm.in', 'http://www.viralcrm.in', 'https://viralcrm.in', 'http://viralcrm.in'],
+  origin: ['http://localhost:5174', 'http://localhost:5173', 'https://www.viralcrm.in', 'http://www.viralcrm.in', 'https://viralcrm.in', 'http://viralcrm.in'],
   credentials: true,               
 }));
 
